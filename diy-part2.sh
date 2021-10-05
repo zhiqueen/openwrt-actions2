4df8317a0bbfae2a0e03b18c58d1cf9d8abf9e38
zhiqueen <zhiqueen@sina.com>
2021-10-05 23:45:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,4 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 # 修改默认主题为argon
-sed -i s/luci-theme-bootstrap/luci-theme-argon/g feeds/luci/collections/luci/Makefile
-# 修改密码为空，自定义名称
-ZZZ=package/lean/default-settings/files/zzz-default-settings
-sed -i /CYXluq4wUazHjmCDBCqXF/d $ZZZ
-sed -i s/OpenWrt /Xiaozao Game Studio compiled in $(TZ=UTC-8 date +%Y.%m.%d) @ OpenWrt /g $ZZZ（#为自定义内容）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
